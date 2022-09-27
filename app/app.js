0ea3319f1a0e1ee61851c37423dada8ad0f4704a
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2022-09-27 12:39:52 -0500

    applying functions to html for vue to execute

--- app/app.js
+++ app/app.js
@@ -4,6 +4,12 @@ const app = Vue.createApp({
             courseGoal: 'Finish the course and learn Vue!',
             vueLink: 'https://vuejs.org/'
         };
+    },
+    methods: {
+        outputGoal() {
+            const randomNumber = Math.floor(Math.random() * 20);
+            return randomNumber
+        }
     }
 });
 
